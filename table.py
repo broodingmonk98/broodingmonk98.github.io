b50321a32abcdf725ca4912dae3ff984c34c4db0
broodingmonk98 <es16btech11014@iith.ac.in>
2018-05-06 19:55:10 +0530

    test

--- table.py
+++ table.py
@@ -3,7 +3,19 @@ import numpy as np
 import html5lib
 
 Math = {'Course':["Linear Algebra","Measure and Integration","Topology","Fourier Analysis"],"Grade":["A","TBA","TBA","TBA"]}
+CSE  = {'Course':["Computational Complexity","Discrete Math"],"Grade":["TBA","A"]}
+EE   = {'Course':["Statistical Learning Theory","Error Correcting Codes","Information Theory"],"Grade":["TBA","A","B-"]}
+Books_Reading = []
+Books_Read = []
+Books_Queued=[]
+
+fp = open('index.html','w')
 Math = pd.DataFrame(Math);
 Math = Math.to_html()
-fp = open('data.html','w')
 fp.write(Math)
+CSE = pd.DataFrame(CSE);
+CSE = CSE.to_html()
+fp.write(CSE)
+EE  = pd.DataFrame(EE);
+EE  = EE.to_html()
+fp.write(EE)
